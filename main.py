df5d3a964193a56005fb2a6a15c32a07e960591e
dq1Mango <thomas@ranney.com>
2024-10-30 12:13:20 -0400

    first commit pt 2!!!

--- main.py
+++ main.py
@@ -33,7 +33,6 @@ label.pack(padx = 5, pady = 5)
 def motion(event):
     x, y = event.x, event.y
     label.config(text = "X:" + str(x) + ", Y:" + str(y))
-    #print('{}, {}'.format(x, y))
 
 root.bind('<Motion>', motion)
 
@@ -41,7 +40,6 @@ def updateCharges(v):
     global charges
     charges[chargeUpdateIndex][2] = int(float(v))
     s.config(label="Magnitude: " + str(charges[chargeUpdateIndex][2]))
-    print(chargeUpdateIndex)
     updateCanvas()
 
 def addCharge(event): #what an accurate name
@@ -49,7 +47,6 @@ def addCharge(event): #what an accurate name
         return
     x, y = event.x, event.y
     if event.state == 8:
-        #print("drew potential")
         drawPotential(x, y, -1)
     else:
         global charges
@@ -70,7 +67,6 @@ def addCharge(event): #what an accurate name
                             global precision
                             precision = 20
 
-                            #print("did the stuffs", i)
                             return
 
         if overlapping:
@@ -105,7 +101,6 @@ def stopChargeMoving(event):
         if precision != maxPrecision - int(precision_slider.get()) + 1:
             precision = maxPrecision - int(precision_slider.get()) + 1
             updateCanvas()
-            print("did this")
         return
 
     uglyGlobalVarible = -1
@@ -167,7 +162,6 @@ def boringUIThingIHadToCodeMyself(event):
     global height
 
     if root.winfo_width() - 2 * xPad != width or root.winfo_height() - 60 - 2 * yPad != height:
-        #print("changing it up")
         precision = 20
         width = event.width - 2 * xPad
         height = event.height - 60 - 2 * yPad
@@ -211,7 +205,6 @@ def calcPotential(x, y):
 
 def unitVector(x):
     length = math.sqrt(x[0] ** 2 + x[1] ** 2)
-    #print(length)
     if length > 1: #shhhhhh
         return [0, 0]
     return (x[0] / length, x[1] / length)
@@ -247,11 +240,7 @@ def drawPotential(x, y, drawDirection):
     x, y = int(x), int(y)
     nextX, nextY = x, y
     ogCoords = [x, y]
-    print("iran")
-    print(ogCoords)
-    #print(ogCoords)
     ogPotential = calcPotential(x, y)
-    #print(ogPotential)
     bestDifference = 100 #holy hacks
     bestCourse = [0, 0]
     for j in range(-1, 2):
@@ -273,37 +262,27 @@ def drawPotential(x, y, drawDirection):
     x, y = nextX, nextY
 
     while not (x == ogCoords[0] and y == ogCoords[1]):
-        print(x, y)
         if not (0 < x < width and 0 < y < height):
             if drawDirection == -1:
-                print("drawing the other way")
                 drawPotential(ogCoords[0], ogCoords[1], 1)
-            print("failed here")
             break
 
         bestDifference = 100 #holy hacks
         bestCourse = [0, 0]
         for i in range(-1, 2):
             for j in range(-1, 2):
-                #print(i, j)
                 if (j == 0 and i == 0) or (i + x == pastX and j + y == pastY):
                     continue
-                #if (i == -1 and j == 1):
-                    #print("grrrr")
                 potential = calcPotential(x + i, y + j)
-                #print(potential)
                 if abs(potential - ogPotential) < bestDifference:
                     bestDifference = abs(potential - ogPotential)
                     bestCourse = [i, j]
-        #print("finished the loop")
         nextX += bestCourse[0]
         nextY += bestCourse[1]
         canvas.create_line((x, y), (nextX, nextY), width = thickness, fill = "white")
         pastX, pastY = x, y
 
         x, y = nextX, nextY
-
-    print("didnt fail ther")
 
 
 def drawCharge(charge):
@@ -324,111 +303,12 @@ def drawCharge(charge):
     radius = defaultRadius + abs(charge[2])
     canvas.create_oval((charge[0] - radius, charge[1] - radius), (charge[0] + radius, charge[1] + radius), fill=fill, outline=outline)
 
-def intToHex(n): #on todays episode of I make my own libraries
-    mapper = ["A", "B", "C", "D", "E", "F"]
-
-    firstDigit = n // 16
-    if firstDigit >= 10:
-        firstDigit = mapper[firstDigit - 10]
-
-    secondDigit = n % 16
-
-    if secondDigit >= 10:
-        secondDigit = mapper[secondDigit - 10]
-
-    return str(firstDigit) + str(secondDigit)
-
-print(intToHex(63))
-
-def calcRedColor(percent):
-    if percent > 1 or percent < 0:
-        print("this happened")
-        return "#FF0000"
-    print("pre hex for red: ", 255 - int((percent * 255)))
-    color = intToHex(255 - int((percent * 255)))
-    #print(percent, color)
-    return "#FF" + color + color
-
-def calcBlueColor(percent):
-    #print(intToHex(255 - int((percent + 0.5) / 2 * 255)))
-    print(percent)
-    print("pre hex for blue: ", int(percent * 255))
-    color = intToHex(255 - int(percent * 255))
-    return "#" + color + color + "FF"
-
-def calcColor(percent):
-    print(percent)
-    if percent > .5:
-        color = intToHex(int((percent * 255)))
-        #print(percent, color)
-        return "#FF" + color + color
-    else:
-        color = intToHex(255 - int(percent * 255))
-        return "#" + color + color + "FF"
-
-def putGusToShame():
-    heatMap = [[0 for i in range(height)] for i in range(width)]
-
-    min = 1
-    max = -1 #this might be a hack idk yet
-    mean = 0 #im sure this will be speedy
-    for x in range(width):
-        for y in range(height):
-            potential = calcPotential(x, y)
-
-            if potential > max:
-                max = potential
-            if potential < min:
-                min = potential
-
-            mean += potential
-            heatMap[x][y] = potential
-
-    mean /= width * height - len(charges)
-
-    stdDev = 0
-    for x in heatMap:
-        for y in x:
-            stdDev += (mean - y) ** 2
-
-    stdDev /= width * height - len(charges) - 1
-    stdDev = stdDev ** (1/2)
-
-    stdMin = (min - mean) / stdDev
-    stdMax = (max - mean) / stdDev
-
-    min = -math.log(1)
-    max = math.log(stdMax + 1)/ math.log(0.1)
-
-    print(max, min)
-    for x in range(width):
-        for y in range(height):
-            #print((float(heatMap[x][y]) - min) / (max - min))
-            #print(calcColor((float(heatMap[x][y]) - min ) / (max - min)))
-            normal = (heatMap[x][y] - mean) / stdDev
-            if normal < 0:
-                pass
-                #canvas.create_line((x, y), (x + 1, y), width=1, fill=calcBlueColor((-math.log(-(normal -stdMin -1)) + 1) / min))
-            else:
-                canvas.create_line((x, y), (x + 1, y), width=1, fill=calcRedColor((math.log(normal + 1)) / math.log(0.1) / (max)))
-            #if heatMap[x][y] < average:
-            #    canvas.create_line((x, y), (x + 1, y), width=1, fill=calcBlueColor((heatMap[x][y] - min ) / (average - min)))
-            #else:
-            #    canvas.create_line((x, y), (x + 1, y), width=1, fill=calcRedColor((heatMap[x][y] - average) / (average - min)))
-
-
-
-#what splendid orginization
-heatMap_button = tk.Button(root, text="put gus to shame", command=putGusToShame)
-heatMap_button.place(x=width/4 + xPad, y = yPad)
-
 def updateCanvas():
     canvas.delete("all")
     for charge in charges:
         if charge[2] == 0: continue
         radius = defaultRadius + abs(charge[2])
         for i in range(fieldDensity + abs(charge[2]) - 1):
-            #print(charge[0] + defaultRadius * math.cos(2 * math.pi * i / fieldDensity), charge[1] + defaultRadius * math.sin(2 * math.pi * i / fieldDensity))
             drawField(charge[0] + radius * math.cos(2 * math.pi * i / (fieldDensity + abs(charge[2]) - 1)), charge[1] + radius * math.sin(2 * math.pi * i / (fieldDensity + abs(charge[2]) - 1)), sign(charge[2]))
 
     for charge in charges:
